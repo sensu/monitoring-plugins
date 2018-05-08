b3d889f9c86adbb20f0399c534c2c56a58a8256e
Caleb Hailey <calebhailey@gmail.com>
2018-05-08 14:34:03 -0700

    hacky workaround to package up libraries needed by a few plugins (e.g. check_http)

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM alpine:latest
-ENV \
-  PLUGINS="check_http"
-RUN apk --update add monitoring-plugins coreutils
+ENV PLUGINS="check_http"
+RUN apk --update add monitoring-plugins coreutils libssl1.0 libintl libcrypto1.0
 ADD create_sensu_asset.sh /build/create_sensu_asset.sh
 WORKDIR /usr/lib/monitoring-plugins/
 CMD sh /build/create_sensu_asset.sh
--- create_sensu_asset.sh
+++ create_sensu_asset.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-BUILD_DIR=/build/monitoring-plugins
-
-mkdir -p $BUILD_DIR/assets
+BUILD_DIR=/tmp/monitoring-plugins
+ASSET_DIR=$BUILD_DIR/assets
+mkdir -p $ASSET_DIR
 
 for plugin in $PLUGINS; do
-  mkdir -p $BUILD_DIR/$plugin/bin $BUILD_DIR/$plugin/lib $BUILD_DIR/$plugin/include
-  cp $plugin $BUILD_DIR/$plugin/bin/
-  tar -zcf $BUILD_DIR/assets/$plugin.tar.gz -C $BUILD_DIR/$plugin .
-  sha512sum $BUILD_DIR/assets/$plugin.tar.gz
+  PKG_DIR=$BUILD_DIR/$plugin
+  mkdir -p $PKG_DIR/bin $PKG_DIR/lib $PKG_DIR/include
+  cp $plugin $PKG_DIR/bin/
+  cp /lib/libssl.so.1.0.0 /usr/lib/libssl.so.1.0.0 $PKG_DIR/lib/
+  cp /usr/lib/libintl.so.8 /usr/lib/libintl.so.8.1.5 $PKG_DIR/lib/
+  cp /lib/libcrypto.so.1.0.0 /usr/lib/libcrypto.so.1.0.0 /usr/lib/engines/libubsec.so /usr/lib/engines/libatalla.so /usr/lib/engines/libcapi.so /usr/lib/engines/libgost.so /usr/lib/engines/libcswift.so /usr/lib/engines/libchil.so /usr/lib/engines/libgmp.so /usr/lib/engines/libnuron.so /usr/lib/engines/lib4758cca.so /usr/lib/engines/libsureware.so /usr/lib/engines/libpadlock.so /usr/lib/engines/libaep.so $PKG_DIR/lib/
+  tar -zcf $ASSET_DIR/$plugin.tar.gz -C $BUILD_DIR/$plugin .
+  sha512sum $ASSET_DIR/$plugin.tar.gz
   cp $BUILD_DIR/assets/$plugin.tar.gz /output/
 done
