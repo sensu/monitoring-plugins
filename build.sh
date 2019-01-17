9e407b14a49ae24a7599591d73c1d8c30f4f1bfa
Caleb Hailey <caleb@sensu.io>
2019-01-17 03:16:41 +0000

    generate more descriptive asset filenames

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 export PLUGINS="check_http,check_ssh"
 export SENSU_GO_ASSET_VERSION="v2.2"
-export SENSU_GO_ASSET_FILENAME="monitoring-plugins-${SENSU_GO_ASSET_VERSION}-linux-x86_64.tar.gz"
 
 mkdir -p assets/alpine assets/debian assets/centos
 
+export SENSU_GO_ASSET_FILENAME="monitoring-plugins-alpine-${SENSU_GO_ASSET_VERSION}-linux-x86_64.tar.gz"
 sudo docker build --build-arg "PLUGINS=$PLUGINS" --build-arg "SENSU_GO_ASSET_VERSION=$SENSU_GO_ASSET_VERSION" -t monitoring-plugins-alpine:$SENSU_GO_ASSET_VERSION -f Dockerfile.alpine .
-sudo docker run -v "$PWD/assets:/assets" monitoring-plugins-alpine:$SENSU_GO_ASSET_VERSION cp /$SENSU_GO_ASSET_FILENAME /assets/alpine/alpine-$SENSU_GO_ASSET_FILENAME
+sudo docker run -v "$PWD/assets:/assets" monitoring-plugins-alpine:$SENSU_GO_ASSET_VERSION cp /$SENSU_GO_ASSET_FILENAME /assets/alpine/$SENSU_GO_ASSET_FILENAME
 
+export SENSU_GO_ASSET_FILENAME="monitoring-plugins-debian-${SENSU_GO_ASSET_VERSION}-linux-x86_64.tar.gz"
 sudo docker build --build-arg "PLUGINS=$PLUGINS" --build-arg "SENSU_GO_ASSET_VERSION=$SENSU_GO_ASSET_VERSION" -t monitoring-plugins-debian:$SENSU_GO_ASSET_VERSION -f Dockerfile.debian .
-sudo docker run -v "$PWD/assets:/assets" monitoring-plugins-debian:$SENSU_GO_ASSET_VERSION cp /$SENSU_GO_ASSET_FILENAME /assets/debian/debian-$SENSU_GO_ASSET_FILENAME
+sudo docker run -v "$PWD/assets:/assets" monitoring-plugins-debian:$SENSU_GO_ASSET_VERSION cp /$SENSU_GO_ASSET_FILENAME /assets/debian/$SENSU_GO_ASSET_FILENAME
 
-sha512sum assets/alpine/alpine-$SENSU_GO_ASSET_FILENAME
-sha512sum assets/debian/debian-$SENSU_GO_ASSET_FILENAME
+sha512sum assets/**/*.tar.gz
