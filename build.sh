42d9fb9670c16309e42d881c869362c272ba1835
Caleb Hailey <caleb@sensu.io>
2019-01-31 01:00:09 +0000

    add Centos builds

--- build.sh
+++ build.sh
@@ -11,4 +11,8 @@ export SENSU_GO_ASSET_FILENAME="monitoring-plugins-debian-${SENSU_GO_ASSET_VERSI
 sudo docker build --build-arg "PLUGINS=$PLUGINS" --build-arg "SENSU_GO_ASSET_VERSION=$SENSU_GO_ASSET_VERSION" -t monitoring-plugins-debian:$SENSU_GO_ASSET_VERSION -f Dockerfile.debian .
 sudo docker run -v "$PWD/assets:/assets" monitoring-plugins-debian:$SENSU_GO_ASSET_VERSION cp /$SENSU_GO_ASSET_FILENAME /assets/debian/$SENSU_GO_ASSET_FILENAME
 
+export SENSU_GO_ASSET_FILENAME="monitoring-plugins-centos-${SENSU_GO_ASSET_VERSION}-linux-x86_64.tar.gz"
+sudo docker build --build-arg "PLUGINS=$PLUGINS" --build-arg "SENSU_GO_ASSET_VERSION=$SENSU_GO_ASSET_VERSION" -t monitoring-plugins-centos:$SENSU_GO_ASSET_VERSION -f Dockerfile.centos .
+sudo docker run -v "$PWD/assets:/assets" monitoring-plugins-centos:$SENSU_GO_ASSET_VERSION cp /$SENSU_GO_ASSET_FILENAME /assets/centos/$SENSU_GO_ASSET_FILENAME
+
 sha512sum assets/**/*.tar.gz
