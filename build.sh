19ebf46dabddd1e81abe89a8d74758eb943d0719
Caleb Hailey <caleb@sensu.io>
2019-03-30 20:34:42 +0000

    cleaned up the build

--- build.sh
+++ build.sh
@@ -9,13 +9,3 @@ do
   docker cp -L $(docker create monitoring-plugins-${PLATFORM}:$SENSU_GO_ASSET_VERSION true):/$SENSU_GO_ASSET_FILENAME ./assets/${PLATFORM}/
   shasum -a 512 assets/${PLATFORM}/*.tar.gz
 done;
-
-# export SENSU_GO_ASSET_FILENAME="monitoring-plugins-debian-${SENSU_GO_ASSET_VERSION}-linux-x86_64.tar.gz"
-# docker build --build-arg "PLUGINS=$PLUGINS" --build-arg "SENSU_GO_ASSET_VERSION=$SENSU_GO_ASSET_VERSION" -t monitoring-plugins-debian:$SENSU_GO_ASSET_VERSION -f Dockerfile.debian .
-# docker cp -L $(docker create --rm monitoring-plugins-debian:$SENSU_GO_ASSET_VERSION true):/$SENSU_GO_ASSET_FILENAME /assets/debian/$SENSU_GO_ASSET_FILENAME
-#
-# export SENSU_GO_ASSET_FILENAME="monitoring-plugins-centos-${SENSU_GO_ASSET_VERSION}-linux-x86_64.tar.gz"
-# docker build --build-arg "PLUGINS=$PLUGINS" --build-arg "SENSU_GO_ASSET_VERSION=$SENSU_GO_ASSET_VERSION" -t monitoring-plugins-centos:$SENSU_GO_ASSET_VERSION -f Dockerfile.centos .
-# docker cp -L $(docker create --rm monitoring-plugins-centos:$SENSU_GO_ASSET_VERSION true):/$SENSU_GO_ASSET_FILENAME /assets/centos/$SENSU_GO_ASSET_FILENAME
-#
-# sha512sum assets/**/*.tar.gz
